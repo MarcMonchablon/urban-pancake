4f099088cf86ccf535bb985fe071946ae58e6a90
Marc Monchablon <marcmonchablon@live.fr>
2019-05-26 01:54:24 +0200

    Change graph-update flow to prepare multiple representations

--- main.js
+++ main.js
@@ -12,29 +12,37 @@ const BLOCK_COLOR = 'indianred';
 
 // === Main stuff
 
+const ctrl = {
+  graphType: 'by-day-month',
+  graph: null,
+  colorScale: null,
+  data: null
+};
+
 function main() {
+  ctrl.graph = {};
   const svgElt = document.querySelector('svg');
   const svgBBox = svgElt.getBoundingClientRect();
 
   // Compute layout variables ...
-  const svg = {
+  const svg = ctrl.graph.svg = {
     width: svgBBox.width,
     height: svgBBox.height,
     elt: svgElt
   };
-  const chart = {
+  const chart = ctrl.graph.chart = {
     width: svg.width - (PADDING.left + PADDING.right),
     height: svg.height - (PADDING.top + PADDING.bottom),
     offset: { x: PADDING.left, y: PADDING.top },
     node: null
   };
-  const domain = {
+  const domain = ctrl.graph.domain = {
     width: chart.width - Y_AXIS_WIDTH,
     height: chart.height - X_AXIS_HEIGHT,
     offset: { x: Y_AXIS_WIDTH, y: 0 },
     node: null
   };
-  const yAxis = {
+  const yAxis = ctrl.graph.yAxis = {
     width: Y_AXIS_WIDTH,
     height: domain.height,
     offset: { x: Y_AXIS_WIDTH, y: 0 },
@@ -42,7 +50,7 @@ function main() {
     axis: null,
     node: null
   };
-  const xAxis = {
+  const xAxis = ctrl.graph.xAxis = {
     width: domain.width,
     height: X_AXIS_HEIGHT,
     offset: { x: domain.offset.x, y: domain.height },
@@ -95,22 +103,55 @@ function main() {
     const data = rawData.reduce((acc, arr) => [...acc, ...arr], []);
     const maxCommits = data.reduce((max, d) => d.commits > max ? d.commits : max, 0);
     const colorInterpolator = d3.interpolateRgb(BACKGROUND_COLOR, BLOCK_COLOR);
-//    const colorInterpolator = d3.interpolateGnBu;
+    //    const colorInterpolator = d3.interpolateGnBu;
     const colorScale = commits => colorInterpolator(commits/maxCommits);
+
+    ctrl.data = data;
+    ctrl.colorScale = colorScale;
 
     domain.node.selectAll('rect.data-point')
       .data(data)
       .enter()
       .append('rect')
-      .attr('class', 'data-point')
+      .attr('class', 'data-point');
+
+    onGraphUpdate();
+  });
+};
+
+function setGraphType(graphType) {
+  ctrl.graphType = graphType;
+  onGraphUpdate();
+}
+
+function onGraphUpdate() {
+  var graphType = ctrl.graphType;
+  var data = ctrl.data;
+  var graph = ctrl.graph;
+  var domain = graph.domain;
+  var yAxis = graph.yAxis;
+  var xAxis = graph.xAxis;
+  var colorScale = ctrl.colorScale;
+
+  if (graphType === 'by-day-month') {
+    // Draw commits as tiles in 2D plan: Y is days and X is months
+    domain.node.selectAll('rect.data-point')
       .attr('y', function(d) { return yAxis.scale(d.day); })
       .attr('x', function(d) { return xAxis.scale(d.month); })
       .attr('width', xAxis.scale.bandwidth())
       .attr('height', yAxis.scale.bandwidth())
       .attr('stroke', 'none')
       .attr('fill', function(d) { return colorScale(d.commits); });
-  });
-};
+  } else if (graphType === 'by-day') {
+    // Draw commits as bars along y-axis: commits aggregated by day of week
+    // TODO
+  } else if (graphType === 'by-month') {
+    // Draw commits as bars along x-axis: commits aggregated by month
+    // TODO
+  } else {
+    console.warn('[onGraphUpdate] Graph type ‘'+graphType+'’ not recognized.');
+  }
+}
 
 function parseCsvDatum(datum) {
   return DAYS.map(day => ({
